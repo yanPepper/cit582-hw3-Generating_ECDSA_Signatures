7e2d62961abd2e717816befa638402536ff5862a
kevin <kevinq@kevins-MacBook-Air.local>
2021-09-16 15:50:21 -0400

    add function sign()

--- sign.py
+++ sign.py
@@ -4,19 +4,30 @@ from fastecdsa.keys import export_key, gen_keypair
 from fastecdsa import curve, ecdsa, keys, point
 from hashlib import sha256
 
+from random import randint
+
 def sign(m):
 	#generate public key
 	#Your code here
-	public_key = None
+	# this is your sign (private key)
+	private_key,public_key = keys.gen_keypair(curve.SECP256k1)
 
 	#generate signature
-	#Your code here
-	r = 0
-	s = 0
+	#Your code 
+	G = ecdsa.secp256k1 #generator
+	n = G.order() # order of G
+	k = randint(1,n)
+	p1 = k * G
+	x1 = p1.x.value
+	r = x1 % n
+	z = sha256(m.encode('utf-8')).hexdigest()
+	k_inv = pow(k,-1,n)
+	s = (z + r * private_key) * k_inv % n
 
 	assert isinstance( public_key, point.Point )
 	assert isinstance( r, int )
 	assert isinstance( s, int )
 	return( public_key, [r,s] )
+
 
 
